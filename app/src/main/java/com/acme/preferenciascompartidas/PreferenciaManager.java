5c56687352f755df727aa324e42ff524f1631867
juan <juan.orih+git@gmail.com>
2023-10-18 23:28:17 -0600

    Implementacion Finalizada de SharedPreferences.

---
+++ app/src/main/java/com/acme/preferenciascompartidas/PreferenciaManager.java
@@ -0,0 +1,37 @@
+package com.acme.preferenciascompartidas;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenciaManager {
+
+    Context context;
+
+    //Constructor de la clase que recibe Context de otra Clase
+    PreferenciaManager(Context contextRecibido) {
+        this.context = contextRecibido;
+    }
+
+    public void salvarLoginDetalles(String email, String password) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDatos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Email", email);
+        editor.putString("Password", password);
+        editor.commit();
+    }
+
+    public String getEmail() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDatos", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("Email", "");
+    }
+
+    public boolean isUsuarioLogedOut() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDatos", Context.MODE_PRIVATE);
+        boolean isEmailVacio = sharedPreferences.getString("Email", "").isEmpty();
+        boolean isPasswordVacio = sharedPreferences.getString("Password", "").isEmpty();
+
+        return isEmailVacio || isPasswordVacio;
+    }
+
+}
